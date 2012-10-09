4ddb8b04b58b3379b80b3b3c7fbb160d188f5ec3
Alex Forbes <alexforbes@gmail.com>
2012-10-09 17:30:51 +0100

    early version, will open a session when XenAPI imports correctly

--- set_ha.py
+++ set_ha.py
@@ -1,6 +1,8 @@
-import sys, time, argparse
+#!/usr/bin/python
 
-import XenAPI, provision
+import os, sys, inspect, time, argparse, getpass
+
+import lib.XenAPI
 
 # void set_ha_restart_priority (VM ref, string)
 # Set the value of the ha_restart_priority field
@@ -14,10 +16,39 @@ import XenAPI, provision
 # Default value:	0
 # Published in:	XenServer 6.0	The delay to wait before proceeding to the next order in the startup sequence (seconds)
 
+# Build commandline argument parser
 parser = argparse.ArgumentParser(description='Set HA properties of a VM')
-parser.add_argument("--name", help="Name of the VM to manage")
-parser.add_argument("--restart-priority", "-p", type=int, help="Order in which the VM is restarted (1=first, 100=last)")
-parser.add_argument("--restart-delay", "-d", type=int, help="Restart delay")
+parser.add_argument("host", help="Host server")
+parser.add_argument("name", help="Name of the VM to manage")
+parser.add_argument("--priority", "-p", type=int, help="Order in which the VM is restarted (1=first, 100=last)")
+parser.add_argument("--delay", "-d", type=int, help="Restart delay")
+parser.add_argument("--password", help="root password for Xen Server")
 
-args = parser.parseargs
 
+args = parser.parse_args()
+
+print ("VM Name: " + str(args.name))
+print ("HA Priority: " + str(args.priority))
+print ("HA Delay: " + str(args.delay))
+
+if not args.password:
+	pwd = getpass.getpass("Root password: ")
+else:
+	pwd = str(args.password)
+
+if not args.host:
+	# TODO: auto-detect from a list of servers
+	host = raw_input("Xen Host: ")
+
+	# raw_input deprecated in Python 3.x, change to input http://docs.python.org/py3k/whatsnew/3.0.html#builtins
+	#var = raw_input("Enter root password: ")
+else:
+	host = args.host
+
+
+# We have all we need, open a Xen session
+xenurl = "https://" + host
+print "API URL: " + xenurl
+
+session = XenAPI.Session(url)
+session.xenapi.login_with_password(username, password)
