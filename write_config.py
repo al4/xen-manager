5baafc8c803504cb27ffd642685ae7e269718e59
Alex Forbes <alexforbes@gmail.com>
2012-10-10 17:55:01 +0100

    add defaults

--- write_config.py
+++ write_config.py
@@ -9,7 +9,12 @@ config = ConfigParser.RawConfigParser()
 config.add_section('Connection')
 config.set('Connection', 'host', 'xen-a01')
 config.set('Connection', 'username', 'root')
-config.set('Connection', 'password', 'password')
+config.set('Connection', 'password', '')
+
+config.add_section('HA Defaults')
+config.set('HA Defaults', 'restart_priority', "")
+config.set('HA Defaults', 'start_order', "1000")
+config.set('HA Defaults', 'start_delay', "0")
 
 with open('xen_manage.cfg', 'wb') as configfile:
 	config.write(configfile)
