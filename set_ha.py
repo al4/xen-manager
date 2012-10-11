b18aff8eeded0c74c322f77113a35cdfa039eb23
Alex Forbes <alexforbes@gmail.com>
2012-10-11 10:42:16 +0100

    Now runs without error. get_machine_id, set_name, set_delay working

--- set_ha.py
+++ set_ha.py
@@ -22,13 +22,14 @@ host 		= 	config.get('Connection', 'host')		# API sever we're connection to (the
 
 if not args.password:
 	password = config.get('Connection', 'password')		# Don't commit this to VCS...
-	if password = '':
+	print password + str(type(password))
+	if password == None:
 		# Clearly hasn't been set, prompt
 		password = getpass.getpass("Root password: ")
 
 # Get defaults:
 default_restart_priority = config.get('HA Defaults', 'restart_priority')
-default_start_order = config.get('HA Defauls', 'start_order')
+default_start_order = config.get('HA Defaults', 'start_order')
 default_start_delay = config.get('HA Defaults', 'start_delay')
 
 # Set session to None so we can test if it's been initialised
@@ -39,18 +40,25 @@ class virtual_machine:
 	def __init__(self, name):
 		# We set the name on initialisation
 		self.name = name
+		# Does the VM exist in Xen? If so set the class values to match.
+		# TODO
 
-	def set_name(name):
+	#### Setting and Getting methods ###
+	# "setting" methods we are simply taking input and setting the value in the class and Xen
+	# "getting" methods we are computing or parsing the value somehow
+	# "read" methods get the current settings from Xen and set them in the class
+
+	def set_name(self, name):
 		# Simple class to set the name (we don't write this back to Xen, only use it for input)
 		self.name = name
 		return 0
 
-	def set_order(order):
+	def set_order(self, order):
 		self.order = order
 		print "Setting order to " + str(order)
 		session.xenapi.VM.set_order(self.id, str(order))
 
-	def set_priority(priority):
+	def set_priority(self, priority):
 		# Sets the priority by
 		self.priority = priority
 
@@ -58,20 +66,20 @@ class virtual_machine:
 
 		session.xenapi.VM.set_ha_restart_priority(self.id, "restart")
 
-	def set_delay(delay):
+	def set_delay(self, delay):
 		# set the delay attribute
 		self.delay = delay
-		print "Setting start_delay to " + str(delay)
+		print "Setting start_delay for " + self.name + " to " + str(delay)
 		session.xenapi.VM.set_start_delay(self.id, str(delay))	# <- documentation says int but you get FIELD_TYPE_ERROR if you pass an integer here, happy when converted to str
 
 		return 0
 
-	def set_cluster(cluster_name):
+	def set_cluster(self, cluster_name):
 		# set the cluster attribute
 		self.cluster = cluster_name
 		return 0
 
-	def get_cluster():
+	def get_cluster(self):
 		# get the cluster name by reading the CSV file
 		#
 		#
@@ -81,17 +89,20 @@ class virtual_machine:
 		cluster = "foo"
 		self.set_cluster(cluster)
 
+	def get_implant(self):
+		# get the implant from DNS TXT record
+		return 0
 
-	def set_machine_id(id):
+	def set_machine_id(self, id):
 		# Set the machine ID (probably won't know this without calling get_machine_id)
 		self.id = id
 		return 0
 
-	def get_machine_id:
+	def get_machine_id(self):
 		# Function to get the machine ID from Xen based on the name. Names should be unique so we
 		# throw an error if there is more than 1 match.
-		ids = session.xenapi.VM.get_by_name_label(vmname)
-		print vmname + " " + str(ids)
+		ids = session.xenapi.VM.get_by_name_label(self.name)
+		print self.name + " " + str(ids)
 
 		if len(ids) != 1:
 			print "Error: VM name had more than one match"
@@ -101,16 +112,6 @@ class virtual_machine:
 
 		return ids[0]
 
-# # Connection class... necessary?
-# class connection:
-# 	# Defines the connection to the Xen server and provides methods for logging in and logging out
-# 	def __init__(self,host,username,password):
-# 		# do something
-# 	def connect():
-# 	def login():
-# 		# login to the server, returns a session object
-# 	def logout():
-# 		# logout from the server
 
 xenurl = "https://" + host
 print "API URL: " + xenurl
@@ -121,7 +122,10 @@ session.xenapi.login_with_password(username, password)
 
 #### Code goes here...
 
-
+vm = virtual_machine("alex1")
+myid = vm.get_machine_id()
+vm.set_machine_id(myid)
+vm.set_delay(10)
 
 ####
 
